af581754286d89ed7be2b5e90f9c9908f24b3919
Kazuma <21kazuma21@gmail.com>
2024-03-23 16:43:39 +0900

    ドットインストールの学習記録です。

---
+++ vs.js
@@ -0,0 +1,24 @@
+'use strict';
+{
+  let num = 10;
+  const numBackup = num;
+  num = 99;
+  console.log(num);
+  console.log(numBackup);
+
+  const nums = [10,20,30];
+  const numsBackup = [...nums];
+  nums[0] = 99;
+  console.log(nums);
+  console.log(numsBackup);
+  let a = 1000;
+  do {
+    a /= 3;
+  } while(a > 10);
+  console.log(a);
+  function randomNumber (a,b) {
+    let c =  Math.floor(Math.random() * (b - a)) + a;
+    return c;
+  }
+  console.log(randomNumber(1,100));
+}
