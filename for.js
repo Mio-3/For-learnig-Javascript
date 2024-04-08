090c30283dcb7e68c4a32553749ef98de1e98abc
Kazuma <21kazuma21@gmail.com>
2024-04-08 23:34:29 +0900

    ドットインストールで学習した内容です。

---
+++ for.js
@@ -0,0 +1,65 @@
+'use strict';
+{
+  // let s = '';
+  // for(let c = 'A'.charCodeAt(0); c <= 'Z'.charCodeAt(0);c++){
+  //   s += String.fromCharCode(c);
+  // }
+  // console.log(s);
+
+  // let s = '';
+  // for(let c = 0x4E00; c <= 0x9FFF;c++){
+  //   s += String.fromCharCode(c);
+  //   if(c % 16 == 15){
+  //     console.log(s);
+  //     s = '';
+  //   }
+  // }
+
+  // for(let i = 0; i < 10;i++){
+  //   let s = '';
+  //   for(let j = 0; j <= i; j++){
+  //     s += '*';
+  //   }
+  //   console.log(s);
+  // }
+
+  // let z = ['A','B','C','D'];
+  // for(let i = 0; i < z.length;i++){
+  //   console.log(z[i]);
+  // }
+
+  // let n = 10;
+  // let a = [];
+  // for(let i = 0;i < n; i++){
+  //   a[i] = i * i;
+  // }
+  // console.log(a);
+
+  // let a = 1000;
+  // do {
+  //   a /= 3;
+  // } while(a >= 10);
+  // console.log(a);
+  // let n = Math.floor(Math.random()*5)+1;
+  // switch(n){
+  //   case 1:
+  //     console.log('大当たり');
+  //     break;
+  //   case 2:
+  //     console.log('当たり');
+  //     break;
+  //   default:
+  //     console.log('ハズレ');
+  //     break;
+  // }
+  // let a = 1;
+  // while(a <= 100){
+  //   a *= 2;
+  // }
+  // console.log(a);
+  function sum(x){
+    let c = x**2;
+    return c;
+  }
+  console.log(sum(2));
+}
